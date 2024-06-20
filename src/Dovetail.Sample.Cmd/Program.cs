c6521bfaa570ff93608e215e096d1d3eeab18078
David Vreony <dpvreony@users.noreply.github.com>
2024-06-20 20:40:47 +0100

    prep sample

---
+++ src/Dovetail.Sample.Cmd/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dovetail.Sample.Cmd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new SomeAppClass().GetSampleKey());
+        }
+    }
+}
---
+++ src/Dovetail.Sample.Cmd/SomeAppClass.cs
@@ -0,0 +1,10 @@
+﻿using Dovetail.Attributes;
+
+namespace Dovetail.Sample.Cmd
+{
+    [EmbedSyncfusionLicenseKey]
+    public partial class SomeAppClass
+    {
+        public string GetSampleKey() => SYNCFUSION_LICENSE_KEY;
+    }
+}
