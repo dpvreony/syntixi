7e981cf580599bf788d80e77e583bcb73f4c1036
David Vreony <dpvreony@users.noreply.github.com>
2026-02-13 10:50:33 +0000

    fix an issue where pragma directives copied from class where attribute sits

--- src/Syntixi.IntegrationTests/SourceGenerator/EmbedSyncfusionLicenseKey/EmbedSyncfusionLicenseKeyGeneratorTests.cs
+++ src/Syntixi.IntegrationTests/SourceGenerator/EmbedSyncfusionLicenseKey/EmbedSyncfusionLicenseKeyGeneratorTests.cs
@@ -9,8 +9,33 @@ namespace Syntixi.IntegrationTests.SourceGenerator.EmbedSyncfusionLicenseKey
 {
     public sealed class EmbedSyncfusionLicenseKeyGeneratorTests
     {
-        [Fact]
-        public void Test()
+        [InlineData(@"#define SYNTIXI_SYNCFUSION_LICENSE_KEY 1
+namespace SomeNamespace{
+    /// <summary>
+    /// Blazor application class.
+    /// </summary>
+#if SYNTIXI_SYNCFUSION_LICENSE_KEY
+    [Syntixi.Attributes.EmbedSyncfusionLicenseKeyAttribute]
+#endif
+    public partial class App
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref=""App""/> class.
+        /// </summary>
+        public App()
+        {
+#if SYNTIXI_SYNCFUSION_LICENSE_KEY
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(SYNCFUSION_LICENSE_KEY);
+#endif
+        }
+    }
+}",
+            "namespace SomeNamespace\r\n{\r\n    public partial class App\r\n    {\r\n        private const string SYNCFUSION_LICENSE_KEY = \"SOMELICENSEKEY\";\r\n    }\r\n}")]
+        [InlineData(
+            "namespace SomeNamespace{[Syntixi.Attributes.EmbedSyncfusionLicenseKeyAttribute]public partial class TestClass { }}",
+            "namespace SomeNamespace\r\n{\r\n    public partial class TestClass\r\n    {\r\n        private const string SYNCFUSION_LICENSE_KEY = \"SOMELICENSEKEY\";\r\n    }\r\n}")]
+        [Theory]
+        public void Test(string sourceCodeToTest, string expected)
         {
             var globalOptions = new InMemoryAnalyzerConfigOptions();
             globalOptions.Add(
@@ -30,8 +55,11 @@ namespace Syntixi.IntegrationTests.SourceGenerator.EmbedSyncfusionLicenseKey
                             [AttributeUsage(AttributeTargets.Class, Inherited = false)]
                             public sealed class EmbedSyncfusionLicenseKeyAttribute : Attribute;
                         }
-                        """),
-                    CSharpSyntaxTree.ParseText("namespace SomeNamespace{[Syntixi.Attributes.EmbedSyncfusionLicenseKeyAttribute]public partial class TestClass { }}")
+                        """,
+                        cancellationToken: TestContext.Current.CancellationToken),
+                    CSharpSyntaxTree.ParseText(
+                        sourceCodeToTest,
+                        cancellationToken: TestContext.Current.CancellationToken)
                 ],
                 references,
                 cSharpCompilationOptions);
@@ -64,7 +92,7 @@ namespace Syntixi.IntegrationTests.SourceGenerator.EmbedSyncfusionLicenseKey
             var generatedOutput = Assert.Single(result.GeneratedSources);
             var generatedCodeAsString = generatedOutput.SourceText.ToString();
             Assert.Equal(
-                "namespace SomeNamespace\r\n{\r\n    public partial class TestClass\r\n    {\r\n        private const string SYNCFUSION_LICENSE_KEY = \"SOMELICENSEKEY\";\r\n    }\r\n}",
+                expected,
                 generatedCodeAsString);
         }
     }
